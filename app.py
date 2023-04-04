3461fcaf8ee7185d24a35aab971af3c03c8836a5
HSM0000 <88722347+HSM0000@users.noreply.github.com>
2023-04-04 11:35:51 +0900

    최종

--- app.py
+++ app.py
@@ -1,16 +1,60 @@
-from flask import Flask, render_template, request, send_file,g,redirect
-from init_db import init_db,get_db,close_db,add_file,search_show,search_file,add_show,delete_file
+from flask import Flask, render_template, request, send_file,g,redirect,session
+from init_db import init_db,get_db,close_db,add_file,search_show,search_file,add_show,delete_file,find_file,get_login,add_user,find_user,find_name
 import os
 import zipfile
 from datetime import datetime
 import datetime
 import random
+
+
 app = Flask(__name__)
+app.secret_key="@!$qefq34@$1234wefQA#$233ASEDFs"
 
 init_db()
+@app.route('/',methods=['GET','POST'])
+def login():
+    if request.method =='GET' :
+        return render_template("login.html")
+    else:
+        user_id=request.form['login_id']
+        user_password=request.form['login_pw']
+        if not (user_id and user_password) : #둘중 하나라도 입력 안되면
+            return "<script type='text/javascript'>alert('모두 입력해주세요.');document.location.href='/';</script>" 
+        find_id=find_user(g.db,user_id)    #db에서 관리자 아이디 가져오기
+        #db에서 아이디와 비밀번호로 정보 가져오기
+    
+        if (find_id) : #정보가 존재하면
+            if(get_login(g.db, user_id, user_password)):  #true 혹은 false
+                session['user_id']=user_id
+                return redirect("/index")   #관리자 메인 페이지 이동
+            else : 
+                return "<script type='text/javascript'>alert('아이디나 비밀번호가 틀립니다.');document.location.href='/';</script>"
+        else : #정보가 존재하지 않으면 
+            return "<script type='text/javascript'>alert('아이디나 비밀번호가 틀립니다.');document.location.href='/';</script>"
+
+
+@app.route("/logout")
+def logout():
+    session.pop('user_id',None)
+    return redirect("/")
+
+@app.route("/join",methods=['GET','POST'])
+def join():
+    if request.method=='GET':
+        return render_template("join.html")
+    else:
+        user_line=request.form['login_id']
+        user_admin=request.form['login_pw']
+        nickname=request.form['name']
+        if not (user_line and user_admin and nickname) : 
+            return "<script type='text/javascript'>alert('모두 입력해주세요.');document.location.href='/join';</script>" 
+        
+
+        add_user(g.db, user_line, user_admin, nickname)
+        return "<script type='text/javascript'>alert('가입 완료');document.location.href='/';</script>"
 
 #맨 처음 페이지 이때 지금까지의 uploads파일에 있는거 보여주기
-@app.route('/')
+@app.route('/index')
 def index():
     uploads_dir = "uploads"
     files = []
@@ -20,8 +64,14 @@ def index():
             ctime = datetime.datetime.fromtimestamp(os.path.getctime(file_path)).strftime('%d-%m-%y %H:%M') 
             mtime = datetime.datetime.fromtimestamp(os.path.getmtime(file_path)).strftime('%d-%m-%y %H:%M')
             file_show=search_show(g.db,file)
-            file_show=file_show[0]["file_show"]
-            files.append((file, os.path.getsize(file_path), ctime,mtime,file_show))
+            if (file_show):
+                name=find_name(g.db,file_show[0]['user_id'])
+                file_show=file_show[0]["file_show"]
+                name=name[0]['name']
+            else:
+                file_show=0
+                name='없음'
+            files.append((file, os.path.getsize(file_path), ctime,mtime,file_show,name))
     return render_template("list_result.html", files=files)
 
 @app.route('/upload', methods=["POST"])
@@ -34,11 +84,12 @@ def upload():
     search=search_file(g.db,file.filename)
     print(search)
     if search==():
-        add_file(g.db,file.filename,file_type)
+        add_file(g.db,file.filename,file_type,session['user_id'])
     elif search[0]["filename"]==file.filename:
         return "<script type='text/javascript'>alert('파일이 있습니다');document.location.href='/';</script>" 
 
-    return redirect("/")
+    return redirect("/index")
+
 
 @app.route("/compress", methods=["POST"])
 def compress():
@@ -54,9 +105,11 @@ def compress():
 
     #compressed_file = f"compressed_{timestamp}.zip"
     compressed_file = os.path.basename(zip_path)
-    add_file(g.db,compressed_file,zip)
+    add_file(g.db,compressed_file,zip,session['user_id'])
 
     return render_template("list_result.html", compressed_file=compressed_file)
+
+
 @app.route("/delete", methods=["POST"])
 def delete():
     uploads_dir = "uploads"
@@ -69,7 +122,19 @@ def delete():
         return "File deleted successfully."
     else:
         return "File not found."
+    
+@app.route('/delete_all',methods=['POST'])
+def delete_all():
+    uploads_dir = "uploads"
+    files = []
+    for file in os.listdir(uploads_dir):
+        file_path = os.path.join(uploads_dir, file)
+        delete_file(g.db,file)
+        if os.path.exists(file_path):
+            os.remove(file_path)
+    return "삭제완료"
 #
+
 @app.route("/download")
 def download():
     uploads_dir = "uploads"
@@ -77,6 +142,34 @@ def download():
     add=add_show(g.db,compressed_file)
     zip_path = os.path.join(uploads_dir, compressed_file)
     return send_file(zip_path, as_attachment=True)
+
+@app.route("/searchfile", methods=['POST'])
+def searchfile():
+    uploads_dir = "uploads"
+    search=request.form['search']
+    print(search)
+    if not (search):
+        return redirect("/index")
+           
+    find_filename = find_file(g.db, search)
+  
+    if (find_filename) : #정보가 존재하면
+        files=[]
+        for file in os.listdir(uploads_dir):
+            file_path = os.path.join(uploads_dir, file)
+            if os.path.isfile(file_path):
+                ctime = datetime.datetime.fromtimestamp(os.path.getctime(file_path)).strftime('%d-%m-%y %H:%M') 
+                mtime = datetime.datetime.fromtimestamp(os.path.getmtime(file_path)).strftime('%d-%m-%y %H:%M')
+                file_show=search_show(g.db,file)
+                if (file_show):
+                    file_show=file_show[0]["file_show"]
+                else:
+                    file_show=0
+                if file==find_filename[0]['filename']:
+                    files.append((file, os.path.getsize(file_path), ctime,mtime,file_show,session['user_id']))
+        return render_template('list_result.html',files=files)   
+    else : #정보가 존재하지 않으면 
+        return "<script type='text/javascript'>alert('파일이 없습니다');document.location.href='/';</script>"
 
 @app.before_request # 요청이 오기 직전에 db 연결
 def before_request():
--- init_db.py
+++ init_db.py
@@ -17,9 +17,10 @@ def init_db():
     db.select_db('mydb')
     
     with db.cursor() as cursor: #DB가 없으면 만들어라.
-        sql1= "CREATE TABLE IF NOT EXISTS mydb.`uploadfile` (`filenumber` int(100) NOT NULL AUTO_INCREMENT, `filename` VARCHAR(100) NOT NULL, `filetype` VARCHAR(100) NOT NULL ,`file_show` INT(100),`date` TIMESTAMP DEFAULT NOW(),PRIMARY KEY (`filenumber`))"
-
-        cursor.execute(sql1)   
+        sql1= "CREATE TABLE IF NOT EXISTS mydb.`uploadfile` (`filenumber` int(100) NOT NULL AUTO_INCREMENT, `filename` VARCHAR(100) NOT NULL, `filetype` VARCHAR(100) NOT NULL ,`file_show` INT(100),`user_id` VARCHAR(100),`date` TIMESTAMP DEFAULT NOW(),PRIMARY KEY (`filenumber`))"
+        sql2= "CREATE TABLE IF NOT EXISTS mydb.`user` (`user_id` VARCHAR(100) NOT NULL,`user_pw` VARCHAR(100) NOT NULL, `name` VARCHAR(100) NOT NULL,PRIMARY KEY (`user_id`) )"
+        cursor.execute(sql1)  
+        cursor.execute(sql2)   
 
     db.commit
     db.close
@@ -37,10 +38,10 @@ def close_db(): #db 연결 종료
 
 
 #파일 이름, 파일 타입, 입력하기
-def add_file(db, filename, filetype):
+def add_file(db, filename, filetype,user_id):
     with db.cursor() as cursor:
-        sql = "insert into mydb.uploadfile values(NULL,%s, %s,0,DEFAULT)"
-        data = (filename, filetype) 
+        sql = "insert into mydb.uploadfile values(NULL,%s, %s,0,%s,DEFAULT)"
+        data = (filename, filetype,user_id) 
         cursor.execute(sql, data)
     db.commit()
 
@@ -55,7 +56,7 @@ def add_show(db,filename):
 #조회수 가져오기
 def search_show(db,filename):
     with db.cursor() as cursor:
-        sql="select file_show from mydb.uploadfile where filename=%s"
+        sql="select * from mydb.uploadfile where filename=%s"
         data=(filename)
         cursor.execute(sql,data)
         result = cursor.fetchall()
@@ -76,3 +77,57 @@ def delete_file(db, filename):
         data = (filename)
         cursor.execute(sql, data)
         db.commit()
+#파일 가져오기
+def find_file(db,filename):
+    with db.cursor() as cursor:
+        sql="select * from mydb.uploadfile where filename=%s"
+        data=(filename)
+        cursor.execute(sql,data)
+        result = cursor.fetchall()
+        return result
+    
+#아이디 찾기
+def find_user(db, user_id):
+    with db.cursor() as cursor:
+        sql= "select user_id from mydb.user where user_id=%s"
+        cursor.execute(sql, user_id)
+        result = cursor.fetchall()
+        #db에 id가 존재함
+        return result    
+
+def find_name(db, user_id):
+    with db.cursor() as cursor:
+        sql= "select name from mydb.user where user_id=%s"
+        cursor.execute(sql, user_id)
+        result = cursor.fetchall()
+        #db에 id가 존재함
+        return result    
+    
+#admin id로 pw 찾아서 db와 비교후 반환 반환 값 true 또는 false
+def get_login(db,user_id, user_pw):
+    with db.cursor() as cursor:
+        sql = "select user_pw from mydb.user where user_id=%s"
+        data = (user_id) 
+        cursor.execute(sql, data)
+        db_password = cursor.fetchone() #db에 저장되어있는 비밀번호    
+        bytes_db_password=db_password['user_pw'].encode('utf-8')
+        bytes_admin_password=user_pw.encode('utf-8')
+        
+        result = bcrypt.checkpw(bytes_admin_password ,bytes_db_password )           
+    return result    #일치하면 true 반환
+
+#admin 계정 추가
+def add_user(db, admin_id, admin_pw,name):
+    with db.cursor() as cursor:
+
+        if (find_user(db,admin_id)):        #db에 이미 계정이 존재하면
+            return 
+        else:
+            encode_pw=admin_pw.encode('utf-8') #bytes 타입 변환
+            salt = bcrypt.gensalt()
+            hashed_pw=bcrypt.hashpw(encode_pw, salt) #해쉬키로 암호화
+            decode_pw = hashed_pw.decode()   #db에 저장하기 전 unicode로 타입 변환
+            sql = "insert into mydb.user values(%s, %s, %s)"
+            data = (admin_id, decode_pw, name) 
+            cursor.execute(sql, data)
+            db.commit()
